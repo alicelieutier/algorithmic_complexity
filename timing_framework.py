11879b340ed82d49d5dc3d2b8957c4f1d39035eb
Alice Lieutier <alice@lieutier.me>
2020-04-30 19:10:29 +0100

    simple example

--- timing_framework.py
+++ timing_framework.py
@@ -2,7 +2,11 @@ import time
 
 start_time = time.time_ns()
 
-#  run some code
+# compare
+# (i*i-2 for i in range(10000000)) # 3000 ns
+# and
+# [i*i-2 for i in range(10000000)] # 1663136000 ns
+
 
 end_time = time.time_ns()
 duration_ns = end_time - start_time
